2fc034b7144e1eca45b64729a9ebd1816f365f44
Andreas Kurm <kurm_de+gitlab@pm.me>
2021-02-24 20:01:02 +0100

    Implemented GridList to show uploaded images

--- src/comps/Attachments.jsx
+++ src/comps/Attachments.jsx
@@ -1,28 +1,60 @@
 import React from 'react';
-
 import FileUpload from './FileUpload';
 import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
-export default class Attachments extends React.Component {
+class Attachments extends React.Component {
     render(){
-        const thumbs = this.props.files.map ((item, index) => {
-            return(
-                <img src={"./images/" + item} width="200"/>
-            );
-        });
+        const getGridListCols = () => {
+            if (isWidthUp('xl', this.props.width)) {
+              return 5;
+            }
+        
+            if (isWidthUp('lg', this.props.width)) {
+              return 4;
+            }
+        
+            if (isWidthUp('md', this.props.width)) {
+              return 3;
+            }
+        
+            return 2;
+          }
+
         return (
-            <Grid container>
+            <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <FileUpload
                         handleUploadInputChange={this.props.handleUploadInputChange}
                     />
                 </Grid>
                 <Grid item xs={12}>
-                    {thumbs}
+                <GridList cellHeight={140} cols={getGridListCols()}>
+                    {this.props.files.map((tile) => (
+                    <GridListTile key={tile} cols={1}>
+                        <img src={"./images/" + tile} alt={tile} />
+                        <GridListTileBar                       
+                        actionIcon={
+                            <IconButton color="secondary">
+                                <DeleteIcon />
+                            </IconButton>
+                        }
+                        />
+                    </GridListTile>
+                    ))}
+                </GridList>
                 </Grid>
             </Grid>
         );
     }
 
 }
+
+
+export default withWidth()(Attachments);
