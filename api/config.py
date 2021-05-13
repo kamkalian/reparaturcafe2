f3701274f44833ecb681a7f67c6a455aca05eb48
Andreas Kurm <kurm_de+gitlab@pm.me>
2021-05-13 14:31:53 +0200

    Implemented TestConfig

--- api/config.py
+++ api/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from formelsammlung.envvar import getenv_typed
 
 
 APP_BASEDIR = Path(__file__).parents[1]
@@ -13,10 +14,16 @@ else:
     print("WARNING: No .env file found.")
 
 
-class Config(object):
+class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
     UPLOAD_FOLDER = Path(APP_BASEDIR, "public/images")
     ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
 
+
+class TestConfig():
+    TESTING = True
+    SECRET_KEY = getenv_typed("SECRET_KEY", "fddsakdljdsanbfeilufh")
+    SQLALCHEMY_DATABASE_URI = getenv_typed("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
