7f874e695cbb7f7b9b8794980469f1afefc3dbde
Andreas Kurm <kurm_de+gitlab@pm.me>
2021-05-15 08:20:34 +0200

    Renamed NewTask folder into taks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import NewTask from './comps/NewTask/NewTask';
-import Database from './comps/Database/Database';
+import NewTask from './comps/task/NewTask';
+import NewUserForm from './comps/user/NewUserForm';
 import QRCodeScanner from './comps/QRCodeScanner';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
