779af562f953214912f0c53bf23e4132816823f9
Andreas Kurm <kurm_de+gitlab@pm.me>
2021-05-18 12:14:45 +0200

    Implemented flask-session to login with qrcode

--- api/__init__.py
+++ api/__init__.py
@@ -5,9 +5,11 @@ from flask_migrate import Migrate
 from flask_jwt_extended import (
     JWTManager
 )
+from flask_session import Session
 
 db = SQLAlchemy()
 migrate = Migrate()
+session = Session()
 
 def create_app(config_class=None):
     """Create app instance."""
@@ -20,6 +22,7 @@ def create_app(config_class=None):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    session.init_app(app)
 
     jwt = JWTManager(app)
 
