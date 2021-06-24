ed0787def383e599c7849886b30cd9c1656291bc
kamkalian <kurm_de+gitlab@pm.me>
2021-06-24 07:16:27 +0200

    Fixed upload folder path

--- api/config.py
+++ api/config.py
@@ -19,7 +19,7 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-    UPLOAD_FOLDER = Path(APP_BASEDIR, "public/images")
+    UPLOAD_FOLDER = Path(APP_BASEDIR, "build/images")
     ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
 
     SECRET_KEY = getenv_typed("SECRET_KEY", "DiesisteinDummyKey")
