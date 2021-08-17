6c7e3e388f9b7b04d95ce474336c3d067fd134e3
kamkalian <kurm_de+gitlab@pm.me>
2021-08-17 19:10:23 +0200

    Removed obsolete imports

--- src/comps/QRCodeScanner.jsx
+++ src/comps/QRCodeScanner.jsx
@@ -4,11 +4,6 @@ import CropFreeIcon from '@material-ui/icons/CropFree';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import PinRequest from './user/PinRequestDialog';
 
 
