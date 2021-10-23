2812441397dbb19735be6c3e23ce7edf6bbd64e1
kamkalian <kurm_de+gitlab@pm.me>
2021-10-23 08:35:58 +0200

    Removed admin_available route

--- api/user_manager/routes.py
+++ api/user_manager/routes.py
@@ -7,26 +7,6 @@ import hashlib
 from datetime import datetime
 
 
-@bp.route('/api/admin_available', methods=['POST', 'GET'])
-def admin_available():
-    resp = {}
-    resp_json = ""
-
-    if request.method == "POST":
-        admin_list = User.query.filter_by(usr_role="admin").all()
-        if len(admin_list) > 0:
-            resp["admin_available"] = True
-        else:
-            resp["admin_available"] = False
-    else:
-        resp["state": "error"]
-        resp["msg": "Not a POST request"]
-
-    resp_json = jsonify(resp)
-
-    return resp_json
-
-
 @bp.route('/api/userlist', methods=['POST', 'GET'])
 def userlist():
     resp = {}
