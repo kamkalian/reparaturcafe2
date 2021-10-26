c3668036827c7fbd170e753584bf718d9595261d
kamkalian <kurm_de+gitlab@pm.me>
2021-10-26 19:44:05 +0200

    Changed wording

--- src/comps/database/Database.jsx
+++ src/comps/database/Database.jsx
@@ -129,7 +129,7 @@ export default class Database extends React.Component {
     
     return(
       <React.Fragment>
-        <h2>Database</h2>
+        <h2>Datenbank</h2>
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <FilterArea
