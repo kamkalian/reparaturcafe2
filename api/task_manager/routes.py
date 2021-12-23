5176f2fc89b51e335c8ede3b6fa2432580ca62ed
kamkalian <kurm_de+gitlab@pm.me>
2021-12-23 17:18:14 +0100

    Changed exp timedelta to 300 minutes

--- api/task_manager/routes.py
+++ api/task_manager/routes.py
@@ -402,7 +402,7 @@ def _is_granted():
     if user:
         if user[3] == "admin" or user[3] == "user":
             today_date = datetime.now()
-            exp_datetime = user[2] + timedelta(minutes=10)
+            exp_datetime = user[2] + timedelta(minutes=300)
             if today_date > exp_datetime:
                 return False
             return True
